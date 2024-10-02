72320f7603b15bcfaeb6385abe4b647131c9b19b
TEACHER\ICT02_22 <admin@admin.com>
2024-10-02 14:33:58 +0900

    라우터 링크 완성

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import routes from '../route/Routes'
 
 const HeaderNav = () => {
   return (
     <div className="text-center mt-3">
-      <Link to={'/'}>Home</Link> |<Link to={'/board'}>board</Link> |
-      <Link to={'/profile'}>Profile</Link> |
+      {routes.map((route, index) => (
+        <Link key={route.path || index} to={route.path}>
+          {' '}
+          | {route.title}
+        </Link>
+      ))}
     </div>
   )
 }
