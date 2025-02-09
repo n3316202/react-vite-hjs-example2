f5814eb5656622e7386861151e095bc7e2af8485
heo <admin@admin.com>
2025-02-09 14:36:00 +0900

    refactor: clean up formatting and syntax in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
 
 function App() {
-  
-  const hello = "Hello";
+    const hello = 'Hello';
 
-  const style ={
-    fontSize:30
-  }
+    const style = {
+        fontSize: 30,
+    };
 
-  return (
-    <>
-     <div style={style}>{hello}</div>
-     <div>{hello}</div>
-    </>
-  )
+    return (
+        <>
+            <div style={style}>{hello}</div>
+            <div>{hello}</div>
+        </>
+    );
 }
 
-export default App
+export default App;
