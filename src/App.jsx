ed34a6364b5d1a8556aed4b4f3565c4c132ede9d
heo <admin@admin.com>
2025-02-10 00:16:19 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import BootStrapButton from './BootstrapExample'
-import Header from './components/Header'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import BoardPage from './pages/BoardPage'
 import ProfilePage from './pages/ProfilePage'
 
 function App() {
+  // http://localhost:5173/#/board
+  // http://localhost:5173/#/profile
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/board" element={<BoardPage />}></Route>
           <Route path="/profile" element={<ProfilePage />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
