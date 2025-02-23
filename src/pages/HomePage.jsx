42fd7047bc90520b027d894e2a8ac1d201fc793f
heo <admin@admin.com>
2025-02-23 11:31:02 +0900

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
-    <>
+    <div className="d-flex flex-column align-items-center">
       <h1 className="text-center mt-5">메인페이지 입니다.</h1>
       <Link to="/profile">프로필 페이지</Link>
+      <br />
       <Link to="/board">보드 페이지</Link>
-    </>
+    </div>
   )
 }
 
--- src/route/Routes.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-
-export default [
-  {
-    path: '/',
-    component: Home,
-  },
-]
